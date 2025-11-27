a812f7a6e6ac3c8c7541700e5a30344c46c05bd5
Bogdan <mitkobogdan@gmail.com>
2025-11-27 22:24:48 +0300

    конструктор, геттер и сеттер надо сделать sendMessage

--- Messenger/src/User.java
+++ Messenger/src/User.java
@@ -1,3 +1,17 @@
 public class User {
-	public String name;
+	private String name;
+    private static int id = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name) {
+        this.name = name;
+
+    }
 }
