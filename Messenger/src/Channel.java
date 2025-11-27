1945f25807e6e7d5c8864331a2bc52da7e6deed7
PeopledSoda <idiatov2007@mail.ru>
2025-11-27 19:33:16 +0300

    создание каналов(в процессе)

--- Messenger/src/Channel.java
+++ Messenger/src/Channel.java
@@ -1,3 +1,26 @@
+
+
+
+import java.util.HashSet;
+
 public class Channel {
 
+    public String name;
+    public User owner;
+    public HashSet<User> listUsers;
+
+
+    public Channel(User owner,String name){
+        this.name=name;
+        this.owner=owner;
+        this.listUsers=new HashSet<>();
+    }
+
+    public void addUser(User user){
+        listUsers.add(user);
+    }
+    public void printSubscribers(){
+        System.out.println("Количество подписчиков" + listUsers.size());
+    }
 }
+
