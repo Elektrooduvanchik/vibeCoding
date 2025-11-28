144386621cda3674134bbd7282825505b14459b9
ADarkSeT <alexzax8062@gmail.com>
2025-11-28 17:27:23 +0300

    Первый вариант 28.11.2025 17:08
    Обявление: groupName, listUsers
    Конструктор: groupName, listUsers
    Методы: addUser, printUsers, printMessages

---
+++ src/main/java/Group.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Group {
+
+    public String groupName;
+    public User[] listUsers;
+
+    public Group(String groupName){
+        this.groupName = groupName;
+        listUsers = new User[0];
+    }
+
+    public void addUser(User user){
+        listUsers = Arrays.copyOf(listUsers, listUsers.length + 1);
+        listUsers[listUsers.length -1] = user;
+    }
+
+    public void printUsers(){
+        System.out.println("Количество участников: " + listUsers.length);
+        for (User user : listUsers){
+            System.out.println(user);
+        }
+    }
+
+    public void printMessages(){
+        for (Message varMessage : message){
+            System.out.println(varMessage.id + " - " + varMessage.getFrom()
+                    + " → " + varMessage.getTo() + ": " + varMessage.getSoobshcheniye());
+        }
+    }
+}
