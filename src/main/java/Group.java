4e646550c03cfae5206c9148e864a66878fe3487
ADarkSeT <alexzax8062@gmail.com>
2025-11-28 22:05:10 +0300

    v2 28.11.2025
    инкапсуляция: groupName, listUsers
    Объявление: message
    printUsers: вывод в одну строчку

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -2,30 +2,40 @@ import java.util.Arrays;
 
 public class Group {
 
-    public String groupName;
-    public User[] listUsers;
+    private String groupName;
+    private User[] listUsers;
 
-    public Group(String groupName){
+    public Message[] message = new Message[0];
+
+    public Group(String groupName) {
         this.groupName = groupName;
         listUsers = new User[0];
     }
 
-    public void addUser(User user){
+    public void addUser(User user) {
         listUsers = Arrays.copyOf(listUsers, listUsers.length + 1);
         listUsers[listUsers.length -1] = user;
     }
 
     public void printUsers(){
         System.out.println("Количество участников: " + listUsers.length);
-        for (User user : listUsers){
-            System.out.print(user + "  " + user.getid + "| ");
+        for (User user : listUsers) {
+            System.out.print(user + "  |  ");
         }
     }
 
     public void printMessages(){
-        for (Message varMessage : message){
+        for (Message varMessage : message) {
             System.out.println(varMessage.id + " - " + varMessage.getFrom()
                     + " → " + varMessage.getTo() + ": " + varMessage.getSoobshcheniye());
         }
+    }
+
+    public String getName() {
+        return groupName;
+    }
+
+    public void setName(String groupName) {
+        this.groupName = groupName;
     }
 }
